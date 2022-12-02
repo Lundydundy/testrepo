6abf31372c9491f55d3eac359b18af3d1fdc1ccc
unknown <matteogiliotti@gmail.com>
2022-12-02 15:42:04 +0000

    Add files

---
+++ hello.py
@@ -0,0 +1,14 @@
+import re
+from
+
+try:
+    x = int(input("x: "))
+except ValueError:
+    print("That is not an int!")
+    exit()
+try:
+    y = int(input("y: "))
+except ValueError:
+    print("That is not an int!")
+    exit()
+print(x + y)
